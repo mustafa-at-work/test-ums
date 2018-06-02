05771cce74cff06e3efbae46a6185f691cd2d63c
danishARZ <zikrullah@ops.netlinks.af>
2018-06-02 11:44:07 +0430

    i have created master table for building, floor, class_room and year

--- addons/nl_ums_master_data/__manifest__.py
+++ addons/nl_ums_master_data/__manifest__.py
@@ -25,6 +25,10 @@
     # always loaded
     'data': [
             'views/master_view.xml',
+            'views/building_view.xml',
+            'views/floor_view.xml',
+            'views/class_room_view.xml',
+            'views/year_view.xml',
             'views/course_view.xml',
      
     ],
