d3732b1eb406062229d2c8067d42e80aea16c0a1
danishARZ <zikrullah@ops.netlinks.af>
2018-06-05 09:37:46 +0430

    Updated the classes model and views

--- addons/nl_ums/models/__init__.py
+++ addons/nl_ums/models/__init__.py
@@ -3,4 +3,3 @@
 from . import models
 from . import classes
 from . import students
-from . import sections
--- addons/nl_ums/models/classes.py
+++ addons/nl_ums/models/classes.py
@@ -5,16 +5,27 @@ from odoo import models, fields, api
 
 class Classes(models.Model):
     _name = 'ums.classes'
+ 
+    # department_id=fields.Many2one('ums.department', string='Department', required=True)
+    # faculty_id=fields.Many2one('ums.faculty', string='Faculty', required=True)
+    # section_ids=fields.One2many('ums.sections','class_id', string='Sections')
 
-    name = fields.Selection([
+    name = fields.Char('Class Name', required=True)
+    department_id=fields.Many2one('ums.department', string='Department', required=True)
+    semester_id = fields.Many2one('ums.semister', string='Semester', required=True)
+    course_id = fields.Many2one('ums.course', string='Course', required=True)
+    lecturer_id = fields.Many2one('res.partner', string='Lecturer', required=True)
+    education_year = fields.Many2one('ums.year',required=True, string='Education Year')
+    timing=fields.Many2one('ums.timing', string='Timing', required=True)
+    start_time = fields.Char('Start time')
+    end_time = fields.Char('End time')
+    class_room=fields.Many2one('ums.class_room', string='Class room', required=True)
+    
+    year = fields.Selection([
         ('firstYear','First Year'),
         ('secondYear','Second Year'),
         ('thirdYear','Third Year'),
         ('fourthYear','Fourth Year'),
     ], default='firstYear')
 
-
-    department_id=fields.Many2one('ums.department', string='Department', required=True)
-    faculty_id=fields.Many2one('ums.faculty', string='Faculty', required=True)
-    section_ids=fields.One2many('ums.sections','class_id', string='Sections')
-     
+    student_ids=fields.Many2many('ums.students', string='Studetns')
