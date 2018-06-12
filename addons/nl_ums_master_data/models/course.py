04e78089166ad8be97186ad675f74a32ff36588f
Mitra Sadat <Mitra Sadat>
2018-06-12 09:40:22 +0430

    Last updated in course model and view.

--- addons/nl_ums_master_data/models/course.py
+++ addons/nl_ums_master_data/models/course.py
@@ -9,6 +9,8 @@ class Course(models.Model):
     _description = 'Course'
     _rec_name = "title"
 
+    topic_ids = fields.One2many('ums.topic', 'course_id', string='Topics')
+
     title = fields.Char('Title', required=True)
     credit = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], string="Credit")
     description = fields.Text('Description')
